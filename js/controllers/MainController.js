df6a122172d9bd2055a45726860197d2089fc20a
Dave Charnuska <davecharnuska@gmail.com>
2016-07-15 13:07:44 -0400

    replace mapMarkers with result of places service call

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,8 @@
-app.controller('MainController', 'places', [ '$scope', 'places', function($scope) {
+app.controller('MainController', [ '$scope', 'places', function($scope, places) {
 	$scope.mapCenter = { lat: 40.741934, lng: -74.004897, zoom: 17 };
 	
 	places.success(function(data) {
-		
+		$scope.geodata = data;
+		$scope.mapMarkers = geodataToMarkers(data);
 	})
 }]);
